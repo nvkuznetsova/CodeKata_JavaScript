b671c2903c5d50564bb28a744831822526784957
Ivan Makarov <Ivan_Makarov@epam.com>
2021-01-09 17:53:26 +0300

    preporation

---
+++ 01_2021/tasks.js
@@ -0,0 +1,71 @@
+// // 0
+// // write a function that swaps value
+
+// const value1='value1'
+// const value2=2
+
+// const swap=(el1, el2)=>{
+
+// }
+
+// swap(value1, value2)
+
+// console.log('value1', value1)
+// console.log('value2', value2)
+
+// // 1
+// // Write 2 functions
+// // 1st transforms string to array
+// // 2nd transfroms array to string
+
+// const array1=['a', 'b', 'c']
+// const arrayToString=(array)=>{
+
+// }
+
+// const string1='abc'
+// const stringToArray=(string)=>{
+
+// }
+// console.log(arrayToString(array1))
+// console.log(stringToArray(string1))
+
+// //2
+// // Write a function that reverses array without using `revese` method
+
+// const array2=[{a:1}, {b:2}, {c:3}]
+
+// const customReverse=(array)=>{
+
+// }
+// console.log(customReverse(array2))
+
+// //3
+// // Write a function that checks equality of two arrays of numbers or strings
+
+// const array3=[...array1]
+// const array4=[...array3, 'a']
+
+// const isEqual =(array1, array2)=>{
+
+// }
+// console.log('should return true', isEqual(array3, array1))
+// console.log('should return false', isEqual(array3, array4))
+
+// //4
+// // write function that returns array with nth element
+
+// const array4=[1,2,3,4,5]
+
+// const deleteEveryCurtainElement=(array=array4, n)=>{
+
+// }
+
+// //5
+// // Write a function that terutn object where key is an element, value is number of times of this element in a array
+
+// const array5=[1, 2, 2, 3, 'EU', 'EU']
+
+// const hashMaker=(array=array5)=>{
+
+// }
